f28735429b4c09f83c9559c185cffd5a505519b4
thebigdizzy <danieltrayler@yahoo.com>
2016-01-20 14:50:09 -0600

    First Cross Platform Commit
    Added includes and cout info

---
+++ Source/main.cpp
@@ -0,0 +1,85 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Jan 20, 2016
+ *      Author: danieltrayler
+ */
+
+#if defined(_WIN32) || (_WIN64)
+#include "SDL.h"
+#endif
+
+#if defined(__linux__)
+#include "SDL2/SDL.h"
+#endif
+
+#if defined(__APPLE__)
+#include "SDL2/SDL.h"
+#endif
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char* argv[]){
+
+#if defined(_WIN32) || (_WIN64)
+	cout << "Running on Windows" << endl;
+#endif
+
+#if defined(__linux__)
+cout << "Running on Linux" << endl;
+
+#endif
+
+#if defined(__APPLE__)
+cout << "Running on Apple" << endl;
+#endif
+
+	SDL_Window *window;                    // Declare a pointer
+
+	SDL_Init (SDL_INIT_VIDEO);              // Initialize SDL2
+
+	// Create an application window with the following settings:
+	window = SDL_CreateWindow("An SDL2 window",                  // window title
+			SDL_WINDOWPOS_UNDEFINED,           // initial x position
+			SDL_WINDOWPOS_UNDEFINED,           // initial y position
+			640,                               // width, in pixels
+			480,                               // height, in pixels
+			SDL_WINDOW_OPENGL                  // flags - see below
+			);
+
+	// Check that the window was successfully created
+	if (window == NULL) {
+		// In the case that the window could not be made...
+		printf("Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+	//Get window surface
+	screenSurface = SDL_GetWindowSurface(window);
+
+	//Fill the surface white
+	SDL_FillRect(screenSurface, NULL,
+			SDL_MapRGB(screenSurface->format, 0, 42, 254));
+
+	//Update the surface
+	SDL_UpdateWindowSurface(window);
+
+	// The window is open: could enter program loop here (see SDL_PollEvent())
+
+	SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+
+	// Close and destroy the window
+	SDL_DestroyWindow(window);
+
+	// Clean up
+	SDL_Quit();
+	return 0;
+}
+
+
