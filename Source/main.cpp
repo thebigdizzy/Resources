d95b4547c7a4cbd0f5dffdd5e26843620e8786fd
thebigdizzy <danieltrayler@yahoo.com>
2016-01-22 14:55:23 -0600

    Added Game States

--- Source/main.cpp
+++ Source/main.cpp
@@ -14,7 +14,7 @@
 #endif
 
 #if defined(__APPLE__)
-#include "SDL2/SDL.h"
+#include "SDL2/SDL.h"0
 #endif
 
 #include <stdio.h>
@@ -40,7 +40,7 @@ int main(int argc, char* argv[]){
 
 	SDL_Window *window;                    // Declare a pointer
 
-	SDL_Init (SDL_INIT_VIDEO);              // Initialize SDL2
+	SDL_Init (SDL_INIT_EVERYTHING);              // Initialize SDL2
 
 	// Create an application window with the following settings:
 	window = SDL_CreateWindow("An SDL2 window",                  // window title
@@ -71,9 +71,294 @@ int main(int argc, char* argv[]){
 	//Update the surface
 	SDL_UpdateWindowSurface(window);
 
-	// The window is open: could enter program loop here (see SDL_PollEvent())
+	//***** Set up a Game Controller variable *****
+	SDL_GameController* gGameController = NULL;
 
-	SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+	//***** Open Game Controller*****
+	gGameController = SDL_GameControllerOpen(0);
+
+	// ***** Turn on Game Controller Events
+	SDL_GameControllerEventState(SDL_ENABLE);
+
+	// ***** SDL Event to handle input
+	SDL_Event event;
+
+	//***** set up variables for the game states
+	enum GameState{MENU, INSTRUCTIONS, PLAYERS1, PLAYERS2, WIN, LOSE};
+
+	// ***** set up the initial state
+	GameState gameState = MENU;
+
+	// bool value to control movement through the states
+	bool menu, instructions, players1, players2, win, lose, quit;
+
+
+	// The window is open: could enter program loop here (see SDL_PollEvent())
+	while (!quit)
+	{
+		switch (gameState)
+		{
+		case MENU:
+			menu = true;
+			cout << "The Game State is Menu" << endl;
+			cout << "Press the A Button for Instructions" << endl;
+			cout << "Press the B Button for 1 Player Game" << endl;
+			cout << "Press the X Button for 2 Player Game" << endl;
+			cout << "Press the Y Button for Quit Game" << endl;
+			cout << endl;
+
+			while(menu)
+			{
+				// check for input events
+				if(SDL_PollEvent(&event)){
+					// check to see if the SDL Window is closed - player clicks X in the Window
+					if(event.type == SDL_QUIT){
+						quit = true;
+						menu = false;
+						break;
+					}
+
+					switch(event.type){
+					case SDL_CONTROLLERBUTTONDOWN:
+						if(event.cdevice.which == 0)
+						{
+							if(event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+							{
+								menu = false;
+								gameState = INSTRUCTIONS;
+							}
+							if(event.cbutton.button == SDL_CONTROLLER_BUTTON_B)
+							{
+								menu = false;
+								gameState = PLAYERS1;
+							}
+							if(event.cbutton.button == SDL_CONTROLLER_BUTTON_X)
+							{
+								menu = false;
+								gameState = PLAYERS2;
+							}
+							if(event.cbutton.button == SDL_CONTROLLER_BUTTON_Y)
+							{
+								menu = false;
+								quit = true;
+							}
+						}
+						break;
+					}
+				}
+			}
+			break; // end main menu case
+
+		case INSTRUCTIONS:
+		instructions = true;
+		cout << "The Game State is Instructions" << endl;
+		cout << "Press the A Button for Main Menu" << endl;
+		cout << endl;
+
+		while(instructions)
+		{
+			// check for input events
+			if(SDL_PollEvent(&event)){
+				// check to see if the SDL Window is closed - player clicks X in the Window
+				if(event.type == SDL_QUIT){
+					quit = true;
+					instructions = false;
+					break;
+				}
+
+				switch(event.type){
+				case SDL_CONTROLLERBUTTONDOWN:
+					if(event.cdevice.which == 0)
+					{
+						if(event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+						{
+							instructions = false;
+							gameState = MENU;
+						}
+					}
+					break;
+				}
+			}
+		}
+		break; // end instructions case
+
+		case PLAYERS1:
+				players1 = true;
+				cout << "The Game State is 1 Player" << endl;
+				cout << "Press the A Button for Win Screen" << endl;
+				cout << "Press the B Button for Lose Screen" << endl;
+				cout << endl;
+
+				while(players1)
+				{
+					// check for input events
+					if(SDL_PollEvent(&event)){
+						// check to see if the SDL Window is closed - player clicks X in the Window
+						if(event.type == SDL_QUIT){
+							quit = true;
+							players1 = false;
+							break;
+						}
+
+						switch(event.type){
+						case SDL_CONTROLLERBUTTONDOWN:
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+								{
+									players1 = false;
+									gameState = WIN;
+								}
+							}
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_B)
+								{
+									players1 = false;
+									gameState = LOSE;
+								}
+							}
+							break;
+						default:break;
+						}
+					}
+				}
+				break; // end main menu case
+
+		case PLAYERS2:
+				players2 = true;
+				cout << "The Game State is 2 Player" << endl;
+				cout << "Press the A Button for Win Screen" << endl;
+				cout << "Press the B Button for Lose Screen" << endl;
+				cout << endl;
+
+				while(players2)
+				{
+					// check for input events
+					if(SDL_PollEvent(&event)){
+						// check to see if the SDL Window is closed - player clicks X in the Window
+						if(event.type == SDL_QUIT){
+							quit = true;
+							players2 = false;
+							break;
+						}
+
+						switch(event.type){
+						case SDL_CONTROLLERBUTTONDOWN:
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+								{
+									players2 = false;
+									gameState = WIN;
+								}
+							}
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_B)
+								{
+									players2 = false;
+									gameState = LOSE;
+								}
+							}
+							break;
+						default:break;
+						}
+					}
+				}
+				break; // end main menu case
+
+		case WIN:
+				win = true;
+				cout << "The Game State is Win Screen" << endl;
+				cout << "Press the A Button for Main Menu" << endl;
+				cout << "Press the B Button for Quit" << endl;
+				cout << endl;
+
+				while(win)
+				{
+					// check for input events
+					if(SDL_PollEvent(&event)){
+						// check to see if the SDL Window is closed - player clicks X in the Window
+						if(event.type == SDL_QUIT){
+							quit = true;
+							win = false;
+							break;
+						}
+
+						switch(event.type){
+						case SDL_CONTROLLERBUTTONDOWN:
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+								{
+									win = false;
+									gameState = MENU;
+								}
+							}
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_B)
+								{
+									win = false;
+									quit = true;
+								}
+							}
+							break;
+						default:break;
+						}
+					}
+				}
+				break; // end main menu case
+
+		case LOSE:
+				lose = true;
+				cout << "The Game State is Lose Screen" << endl;
+				cout << "Press the A Button for Main Menu" << endl;
+				cout << "Press the B Button for Quit" << endl;
+				cout << endl;
+
+				while(lose)
+				{
+					// check for input events
+					if(SDL_PollEvent(&event)){
+						// check to see if the SDL Window is closed - player clicks X in the Window
+						if(event.type == SDL_QUIT){
+							quit = true;
+							lose = false;
+							break;
+						}
+
+						switch(event.type){
+						case SDL_CONTROLLERBUTTONDOWN:
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_A)
+								{
+									lose = false;
+									gameState = MENU;
+								}
+							}
+							if(event.cdevice.which == 0)
+							{
+								if(event.cbutton.button == SDL_CONTROLLER_BUTTON_B)
+								{
+									lose = false;
+									quit = true;
+								}
+							}
+							break;
+						default:break;
+						}
+					}
+				}
+				break; // end main menu case
+		default:break;
+		}
+	}
+
+
+	// SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
 
 	// Close and destroy the window
 	SDL_DestroyWindow(window);
